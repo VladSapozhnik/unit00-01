290d8705c28334453d1ee3ca541f03c184f1bc82
Vlad Sapozhnik <vladbars2@gmail.com>
2025-10-09 20:31:49 +0300

    fixing server

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,25 @@
 import express, { Request, Response } from "express";
 import 'dotenv/config'
-// import {videosRouter} from "./routes/videos-router";
-// import {HTTP_STATUS} from "./constants/http-status";
-// import {AvailableResolutions} from "./enum/available-resolutions";
-// import {db} from "./db";
+import {videosRouter} from "./routes/videos-router";
+import {db} from "./db";
+import {HTTP_STATUS} from "./constants/http-status";
 
 const port: number = Number(process.env.PORT) || 3000;
 const app = express();
 
 app.use(express.json());
 
-
-
 app.get('/', (req: Request, res: Response) => {
     res.send('Main page!!!')
 })
-//
-// app.use('/videos', videosRouter)
-//
-// app.delete('/testing/all-data', (req: Request, res: Response) => {
-//     db.videos.length = 0;
-//     db.videos = [];
-//     res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
-// })
+
+app.use('/videos', videosRouter)
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.videos.length = 0;
+    db.videos = [];
+    res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
+})
 
 app.listen(port, () => {
     console.log('starting to port: ' + port)
